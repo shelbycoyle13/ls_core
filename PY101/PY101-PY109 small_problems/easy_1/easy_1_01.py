84b7e5380772ebfef603cfa7382edc4076835158
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2024-08-23 17:01:30 -0400

    Edited first easy problem, original had test cases I was experimenting with

--- PY101/PY101-PY109 small_problems/easy_1/easy_1_01.py
+++ PY101/PY101-PY109 small_problems/easy_1/easy_1_01.py
@@ -1,14 +1,13 @@
-# # Write a function that takes one integer argument and returns True when the number's absolute value is odd, False otherwise.
+# Write a function that takes one integer argument and returns True when the number's absolute value is odd, False otherwise.
 
-# def is_it_odd(int):
-#     if abs(int) % 2 != 0:
-#         return True
-#     else:
-#         return False
+def is_it_odd(int):
+    if abs(int) % 2 != 0:
+        return True
+    else:
+        return False
     
-# print(is_it_odd(-2))
-
-def is_odd(number):
-    return number % -3
-
-print(is_odd(-4))
+print(is_it_odd(-2))
+print(is_it_odd(2))
+print(is_it_odd(0))
+print(is_it_odd(3))
+print(is_it_odd(-3))
