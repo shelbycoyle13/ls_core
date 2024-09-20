6f07a68d16b41ac9e0fb4349e53e716e81a47814
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2024-09-20 18:25:12 -0400

    Remove files without sub directory

--- PY101/lesson_3/question_08.py
+++
@@ -1,9 +0,0 @@
-# In the previous problem, our first answer added 'Dino' to the list like this:
-
-flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"]
-# flintstones += ["Dino", "Hoppy"]
-flintstones.extend(["Dino", "Hoppy"])
-
-# How can we add multiple items to our list (e.g., 'Dino' and 'Hoppy')? Replace the call to append with another method invocation.
-
-print(flintstones)
--- PY101/lesson_3/question_10.py
+++
@@ -1,7 +0,0 @@
-# Print the following string with the word important replaced by urgent:
-
-advice = "Few things in life are as important as house training your pet dinosaur."
-
-new_string = advice.replace("important", "urgent")
-
-print(new_string)
