7553f5aafc37afde795f569fa0e6cdb570fd988f
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2024-09-20 18:24:15 -0400

    Removed files without sub directory

--- PY101/lesson_3/easy_2/question_06.py
+++
@@ -1,8 +0,0 @@
-# Back in the stone age (before CSS), we used spaces to align things on the screen. If we have a 40-character wide table of Flintstone family members, how can we center the following title above the table with spaces?
-
-title = "Flintstone Family Members"
-
-centered_title = title.center(40)
-
-print(centered_title)
-print(len(centered_title))
--- PY101/lesson_3/easy_2/question_08.py
+++
@@ -1,9 +0,0 @@
-# Determine whether the following dictionary of people and their age contains an entry for 'Spot':
-
-ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 402, 'Eddie': 10}
-
-find_this = ages.get("Spot")
-
-print(find_this)
-
-print("Spot" in ages)
