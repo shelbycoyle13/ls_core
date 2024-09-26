1b02f6ea9f1f05ac08fbe70669eaebfc020ae30b
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2024-09-26 14:03:04 -0400

    Made changes suggested

--- PY101/lesson_2/rock_paper_scissors/rock_paper_scissors.py
+++ PY101/lesson_2/rock_paper_scissors/rock_paper_scissors.py
@@ -3,101 +3,136 @@ rock-paper-scissors-lizard-spock
 against the computer. 
 It announces a winner after either one gets 3 wins."""
 
+import os
 import random
+import time
 
 VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
 VALID_CHOICES_LETTER = ["r", "p", "x", "l", "s"]
 
+def clean_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 def prompt(message):
     print(f"==> {message}")
 
+def banner():
+    print(
+        """
+*******************************************************
+====  Let's play Rock-Paper-Scissors-Lizard-Spock! ==== 
+*******************************************************
+"""
+    )
+def prompt_user_choice():
+    prompt(
+        "Here's how to play:\n"
+        "Rock beats Scissors and Lizard\n"
+        "Paper beats Rock and Spock\n"
+        "Scissors beats Paper and Lizard\n"
+        "Lizard beats Paper and Spock\n"
+        "Spock beats Rock and Scissors\n"
+        f"Choose one: {', '.join(VALID_CHOICES)}.\n"
+        "To make your selection, type in the first letter of the word, "
+        "except for scissors, please use 'x'.\n"
+        "We play until someone wins 3 games. Let's go!"
+    )
+    choice = input().strip().casefold()
+
+    while (choice not in VALID_CHOICES_LETTER) and (
+        choice not in VALID_CHOICES):
+        prompt("That's not a valid choice. Please try again!")
+        choice = input()
+    return match_choice_letter(choice)
 
 def match_choice_letter(letter):
-    if letter == "r":
+    if letter in (VALID_CHOICES[0], VALID_CHOICES_LETTER[0]):
         return "rock"
-    if letter == "p":
+    if letter in (VALID_CHOICES[1], VALID_CHOICES_LETTER[1]):
         return "paper"
-    if letter == "x":
+    if letter in (VALID_CHOICES[2], VALID_CHOICES_LETTER[2]):
         return "scissors"
-    if letter == "l":
+    if letter in (VALID_CHOICES[3], VALID_CHOICES_LETTER[3]):
         return "lizard"
-    if letter == "s":
+    if letter in (VALID_CHOICES[4], VALID_CHOICES_LETTER[4]):
         return "spock"
     return "An error occurred"
 
-def player_wins(player, computer):
-    return (
-        (player == "rock" and computer in ("scissors", "lizard"))
-        or (player == "paper" and computer in ("rock", "spock"))
-        or (player == "scissors" and computer in ("paper", "lizard"))
-        or (player == "lizard" and computer in ("paper", "spock"))
-        or (player == "spock" and computer in ("scissors", "rock"))
-    )
+WINNING_COMBOS = {
+    'rock':     ['scissors', 'lizard'],
+    'paper':    ['rock',     'spock'],
+    'scissors': ['paper',    'lizard'],
+    'lizard':   ['paper',    'spock'],
+    'spock':    ['rock',     'scissors'],
+}
 
-def computer_wins(player, computer):
-    return (
-        (player == "rock" and computer in ("paper", "spock"))
-        or (player == "paper" and computer in ("scissors", "lizard"))
-        or (player == "scissors" and computer in ("rock", "spock"))
-        or (player == "lizard" and computer in ("rock", "scissors"))
-        or (player == "spock" and computer in ("lizard", "paper"))
-    )
+def compute_wins(player_choice, computer_choice):
+    return computer_choice in WINNING_COMBOS[player_choice]
 
 def display_winner(player, computer):
     prompt(f"You chose: {player} and the computer chose: {computer}")
 
-    if player_wins(player, computer):
+    if compute_wins(player, computer):
         return "You win!"
-    if computer_wins(player, computer):
+    if compute_wins(computer, player):
         return "Computer wins!"
     return "It's a tie!"
 
-ANSWER = "y"
-
-PLAYER_WINS = 0
-COMPUTER_WINS = 0
-
-while ANSWER == "y":
-    prompt(
-        f"Choose one: {', '.join(VALID_CHOICES)}. "
-        "To make your selection, type in the first letter of the word, "
-        "except for scissors, please use 'x'."
-    )
-    choice = input().strip()
-
-    while choice not in VALID_CHOICES_LETTER:
-        prompt("That's not a valid choice. Please try again!")
-        choice = input()
-
-    PLAYER_CHOICE = match_choice_letter(choice)
-
-    computer_choice = random.choice(VALID_CHOICES)
-
-    RESULT = display_winner(PLAYER_CHOICE, computer_choice)
-
-    prompt(RESULT)
-
-    if RESULT == "You win!":
-        PLAYER_WINS += 1
-    if RESULT == "Computer wins!":
-        COMPUTER_WINS += 1
-
-    prompt(f"The current score is {PLAYER_WINS} (you)"
-           f" to {COMPUTER_WINS} (computer).")
-
-    if PLAYER_WINS == 3:
-        prompt("You are the grand winner - congratulations!")
-        PLAYER_WINS = 0
-        COMPUTER_WINS = 0
-    elif COMPUTER_WINS == 3:
-        prompt("The computer is the grand winner - "
-               "sorry, you'll win next time!")
-        PLAYER_WINS = 0
-        COMPUTER_WINS = 0
-
+def play_again():
     prompt("Do you want to play again? (Type y for yes and n for no)")
-    ANSWER = input().lower().strip()
+    answer = input().lower().strip()
 
-    while ANSWER not in ["y", "n"]:
+    while answer not in ["y", "n"]:
         prompt("That's not a valid choice. Please try again!")
-        ANSWER = input()
+        answer = input()
+
+    return answer
+
+def play_rps():
+    answer = "y"
+    player_wins = 0
+    computer_wins = 0
+    while answer == "y":
+        clean_screen()
+        banner()
+
+        player_choice = prompt_user_choice()
+
+        computer_choice = random.choice(VALID_CHOICES)
+
+        result = display_winner(player_choice, computer_choice)
+
+        prompt(result)
+
+        if result == "You win!":
+            player_wins += 1
+        if result == "Computer wins!":
+            computer_wins += 1
+
+        prompt(f"The current score is {player_wins} (you)"
+            f" to {computer_wins} (computer).")
+
+        time.sleep(3)
+
+        if player_wins == 3:
+            prompt("You are the grand winner - congratulations!")
+            answer = play_again()
+            if answer == "n":
+                break
+            player_wins = 0
+            computer_wins = 0
+        elif computer_wins == 3:
+            prompt("The computer is the grand winner - "
+                "sorry, you'll win next time!")
+            answer = play_again()
+            if answer == "n":
+                break
+            player_wins = 0
+            computer_wins = 0
+
+        clean_screen()
+
+play_rps()
